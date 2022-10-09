7f357673316275c8b895aebdbc32117165d9303e
Stiiipan <dumn2011@gmail.com>
2022-10-09 20:47:39 +0900

    Detail body Done

---
+++ src/components/DetailBody.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Box, Typography} from "@mui/material";
+import Star from "../images/star.png"
+import Phone from "../images/phone.png"
+import {format} from "date-fns";
+
+const {differenceInYears, parse} = require("date-fns")
+
+function calculateAge(dob) {
+    const date = parse(dob, "yyyy-MM-dd", new Date());
+    console.log(date)
+    return differenceInYears(new Date(), date);
+}
+
+const DetailBody = ({user}) => {
+    const primaryText = {
+        fontWeight: 500,
+        fontSize: "16px",
+        lineHeight: "20px",
+        color: "#050510",
+        fontFamily: 'Inter',
+    }
+    return (
+        <Box p={"8px 16px"}>
+            <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                p: "20px 4px 20px 2px",
+                borderBottom: "1px solid #F7F7F8"
+            }}>
+                <Box sx={{
+                    display: "flex",
+                    alignItems: "center",
+                }}>
+                    <img src={Star} alt="" style={{paddingRight: "14px"}}/>
+                    <Typography sx={primaryText}>{format(user.birthday, "dd MMMM yyyy")}</Typography>
+                </Box>
+                <Typography sx={primaryText}>{calculateAge(user.birthday)} года</Typography>
+            </Box>
+
+            <Box
+                sx={{}}
+            >
+                <Box sx={{
+                    display: "flex",
+                    alignItems: "center",
+                }}>
+                    <img src={Phone} alt="" style={{paddingRight: "14px"}}/>
+                    <Typography sx={{
+                        fontWeight: 500,
+                        fontSize: "16px",
+                        lineHeight: "20px",
+                        color: "#050510"
+                    }}>{user.phone}</Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default DetailBody;
