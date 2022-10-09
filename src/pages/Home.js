6e31036edd8883a49cb850632bd64298a579cd2b
Stiiipan <dumn2011@gmail.com>
2022-10-09 14:51:20 +0900

    Skeleton v. 0.1

---
+++ src/pages/Home.js
@@ -0,0 +1,379 @@
+import React from 'react';
+import {Box, Card, CardContent, CardHeader, CardMedia, Skeleton} from "@mui/material";
+
+const Home = () => {
+
+    const loading = true;
+
+    return (
+        <Box
+            sx={{
+                p: "16px",
+                overflow: "hidden"
+            }}
+        >
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box>
+            <Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+                <Box
+                    sx={{p: "6px 0", mr: "16px"}}
+                >
+                    {
+                        loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                    }
+                </Box>
+                <Box>
+                    <Box>
+                        {
+                            loading ? (
+                                <Skeleton variant="rounded" width={144} height={16}/>
+                            ) : (
+                                'Ted'
+                            )
+                        }
+                    </Box>
+                    <Box mt={"6px"}>
+                        {
+                            loading ?
+                                <Skeleton variant="rounded" width={80} height={12}/>
+                                :
+                                <></>
+                        }
+                    </Box>
+                </Box>
+            </Box><Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+            <Box
+                sx={{p: "6px 0", mr: "16px"}}
+            >
+                {
+                    loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                }
+            </Box>
+            <Box>
+                <Box>
+                    {
+                        loading ? (
+                            <Skeleton variant="rounded" width={144} height={16}/>
+                        ) : (
+                            'Ted'
+                        )
+                    }
+                </Box>
+                <Box mt={"6px"}>
+                    {
+                        loading ?
+                            <Skeleton variant="rounded" width={80} height={12}/>
+                            :
+                            <></>
+                    }
+                </Box>
+            </Box>
+        </Box><Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+            <Box
+                sx={{p: "6px 0", mr: "16px"}}
+            >
+                {
+                    loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                }
+            </Box>
+            <Box>
+                <Box>
+                    {
+                        loading ? (
+                            <Skeleton variant="rounded" width={144} height={16}/>
+                        ) : (
+                            'Ted'
+                        )
+                    }
+                </Box>
+                <Box mt={"6px"}>
+                    {
+                        loading ?
+                            <Skeleton variant="rounded" width={80} height={12}/>
+                            :
+                            <></>
+                    }
+                </Box>
+            </Box>
+        </Box><Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+            <Box
+                sx={{p: "6px 0", mr: "16px"}}
+            >
+                {
+                    loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                }
+            </Box>
+            <Box>
+                <Box>
+                    {
+                        loading ? (
+                            <Skeleton variant="rounded" width={144} height={16}/>
+                        ) : (
+                            'Ted'
+                        )
+                    }
+                </Box>
+                <Box mt={"6px"}>
+                    {
+                        loading ?
+                            <Skeleton variant="rounded" width={80} height={12}/>
+                            :
+                            <></>
+                    }
+                </Box>
+            </Box>
+        </Box><Box sx={{width: "343px", display: "flex", alignItems: "center"}} elevation={0}>
+            <Box
+                sx={{p: "6px 0", mr: "16px"}}
+            >
+                {
+                    loading ? <Skeleton variant="circular" width={72} height={72}/> : <></>
+                }
+            </Box>
+            <Box>
+                <Box>
+                    {
+                        loading ? (
+                            <Skeleton variant="rounded" width={144} height={16}/>
+                        ) : (
+                            'Ted'
+                        )
+                    }
+                </Box>
+                <Box mt={"6px"}>
+                    {
+                        loading ?
+                            <Skeleton variant="rounded" width={80} height={12}/>
+                            :
+                            <></>
+                    }
+                </Box>
+            </Box>
+        </Box>
+        </Box>
+    );
+};
+
+export default Home;
