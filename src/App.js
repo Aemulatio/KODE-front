4d90a7c0baf165b579357db93f8ec02e3fd6c000
Stiiipan <dumn2011@gmail.com>
2022-10-09 21:54:11 +0900

    prefetch data about detail page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import {useState} from "react";
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, createBrowserRouter, RouterProvider} from "react-router-dom";
 import Main from "./pages/Main";
 import Detail from "./pages/Detail";
 import {ru} from 'date-fns/locale'
 import {setDefaultOptions} from "date-fns";
+import axios from "axios";
 
 
 function App() {
     setDefaultOptions({locale: ru})
     const [activeTab, setActiveTab] = useState("all");
     const [search, setSearch] = useState("")
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Main activeTab={activeTab} search={search} setActiveTab={setActiveTab}
+                           setSearch={setSearch}/>,
+        },
+        {
+            path: "/:id",
+            element: <Detail/>,
+            loader: async (params) => {
+                let data;
+                await axios.get(`https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users`, {params: {"id": params.params.id}}).then(
+                    (resp) => {
+                        data = resp.data.items[0]
+                    }
+                )
+                return data
+            },
+        },
+    ])
+
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={
-                    <Main activeTab={activeTab} search={search} setActiveTab={setActiveTab}
-                          setSearch={setSearch}/>
-                }/>
-                <Route path="/:id" element={<Detail/>}/>
-            </Routes>
+            <RouterProvider router={router}/>
         </div>
     );
 }
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import DetailHeader from "../components/DetailHeader";
+import DetailBody from "../components/DetailBody";
+import {
+    useLoaderData,
+} from "react-router-dom";
 
 const Detail = () => {
+    let data = useLoaderData()
+    const user = useMemo(() => data)
+    console.log(user)
     return (
         <div>
-            <p>detail</p>
+            <DetailHeader user={user}/>
+            <DetailBody user={user}/>
         </div>
     );
 };
