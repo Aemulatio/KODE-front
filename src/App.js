9ea27afa88c01447d5e856ffab0e8a3d1399a9d2
Stiiipan <dumn2011@gmail.com>
2022-10-09 16:19:16 +0900

    search state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Home from "./pages/Home";
 
 function App() {
     const [activeTab, setActiveTab] = useState("all");
-
+    const [search, setSearch] = useState("")
     return (
         <div className="App">
-            <TopAppBar activeTab={activeTab} setActiveTab={setActiveTab}/>
-            <Home activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <TopAppBar activeTab={activeTab} setActiveTab={setActiveTab} setSearch={setSearch}/>
+            <Home activeTab={activeTab} search={search}/>
         </div>
     );
 }
