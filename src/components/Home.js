b625785925ecdbd3d0046e1be8214814bee2b01a
Stiiipan <dumn2011@gmail.com>
2022-10-09 17:36:18 +0900

    moved to components

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {Box, Card, CardContent, CardHeader, CardMedia, Skeleton, Typography} from "@mui/material";
 import axios from "axios";
 import EmptySearch from "../components/EmptySearch";
+import {Link} from "react-router-dom";
 
 const Home = (props) => {
     const {activeTab, search} = props;
@@ -58,46 +59,48 @@ const Home = (props) => {
                                     return el
                                 }
                             }).map(user => (
-                                <Box key={user.id} sx={{width: "343px", display: "flex", alignItems: "center"}}
-                                     elevation={0}>
-                                    <Box sx={{p: "6px 0", mr: "16px"}}>
-                                        <img
-                                            style={{width: "72px", height: "72px", borderRadius: "50%"}}
-                                            loading={'lazy'}
-                                            src={user.avatarUrl}
-                                            alt={user.firstName}/>
-                                    </Box>
-                                    <Box>
+                                <Link to={user.id} style={{textDecoration: "none"}}>
+                                    <Box key={user.id} sx={{width: "343px", display: "flex", alignItems: "center"}}
+                                         elevation={0}>
+                                        <Box sx={{p: "6px 0", mr: "16px"}}>
+                                            <img
+                                                style={{width: "72px", height: "72px", borderRadius: "50%"}}
+                                                loading={'lazy'}
+                                                src={user.avatarUrl}
+                                                alt={user.firstName}/>
+                                        </Box>
                                         <Box>
-                                            <Typography
-                                                component={"span"}
-                                                sx={{
-                                                    fontSize: "16px",
-                                                    lineHeight: "20px",
-                                                    color: "#050510",
-                                                }}>
-                                                {user.firstName} {user.lastName}
-                                            </Typography>{" "}
-                                            <Typography
-                                                component={"span"}
-                                                sx={{
-                                                    fontSize: "14px",
-                                                    lineHeight: "18px",
-                                                    color: "#97979B",
-                                                }}>
-                                                {user.userTag}
-                                            </Typography>
-                                        </Box>
-                                        <Box mt={"6px"}>
-                                            <Typography sx={{
-                                                fontSize: "13px",
-                                                lineHeight: "16px",
-                                                fontWeight: 400,
-                                                color: "#55555C"
-                                            }}>{user.position[0].toUpperCase() + user.position.slice(1)}</Typography>
+                                            <Box>
+                                                <Typography
+                                                    component={"span"}
+                                                    sx={{
+                                                        fontSize: "16px",
+                                                        lineHeight: "20px",
+                                                        color: "#050510",
+                                                    }}>
+                                                    {user.firstName} {user.lastName}
+                                                </Typography>{" "}
+                                                <Typography
+                                                    component={"span"}
+                                                    sx={{
+                                                        fontSize: "14px",
+                                                        lineHeight: "18px",
+                                                        color: "#97979B",
+                                                    }}>
+                                                    {user.userTag}
+                                                </Typography>
+                                            </Box>
+                                            <Box mt={"6px"}>
+                                                <Typography sx={{
+                                                    fontSize: "13px",
+                                                    lineHeight: "16px",
+                                                    fontWeight: 400,
+                                                    color: "#55555C"
+                                                }}>{user.position[0].toUpperCase() + user.position.slice(1)}</Typography>
+                                            </Box>
                                         </Box>
                                     </Box>
-                                </Box>
+                                </Link>
                             ))
                         }
                         {
