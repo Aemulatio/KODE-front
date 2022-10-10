40fb3f7c5d782b40150ce76461d8bb9081de5928
Stiiipan <dumn2011@gmail.com>
2022-10-11 00:09:10 +0900

    fix: filter color; Add: search keep it's state

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,7 +6,7 @@ const Main = ({activeTab, setActiveTab, search, setSearch, order, setOrder}) => 
     return (
         <>
             <TopAppBar activeTab={activeTab} setActiveTab={setActiveTab} setSearch={setSearch} setOrder={setOrder}
-                       order={order}/>
+                       order={order} search={search}/>
             <Home activeTab={activeTab} search={search} order={order}/>
         </>
     );
